6f0b473e80206ec8f51ab41cfbf2dedf707e4857
aniket <aniket.umbc@gmail.com>
2024-07-27 13:18:34 +0530

    Basic chat with context

--- src/chatIndex.ts
+++ src/chatIndex.ts
@@ -1,1 +1,38 @@
-console.log(process.env);
+import { OpenAI } from 'openai';
+import { encoding_for_model } from 'tiktoken';
+
+const openai = new OpenAI();
+
+const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+  {
+    role: 'system',
+    content: 'You are helpful chatbot give answers short',
+  },
+];
+
+async function createChatCompletion() {
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: context,
+  });
+
+  const responseMessages = response.choices[0].message;
+
+  context.push({
+    role: 'assistant',
+    content: responseMessages.content,
+  });
+
+  console.log(
+    `${response.choices[0].message.role}: ${response.choices[0].message.content}`
+  );
+}
+
+process.stdin.addListener('data', async function (input) {
+  const userInput = input.toString().trim();
+  context.push({
+    role: 'user',
+    content: userInput,
+  });
+  await createChatCompletion();
+});
