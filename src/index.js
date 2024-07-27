8450d3488116565d4b96ad6fc61cdef55e2ba9bd
aniket <aniket.umbc@gmail.com>
2024-07-27 12:29:36 +0530

    added token pckg

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { OpenAI } from 'openai';
+import { encoding_for_model } from 'tiktoken';
 
 const openai = new OpenAI();
 
@@ -7,13 +8,31 @@ async function main() {
     model: 'gpt-3.5-turbo',
     messages: [
       {
+        role: 'system',
+        content: 'Your response will be like 35 years human',
+      },
+      {
         role: 'user',
-        content: 'What is capital of maharastra?',
+        content: 'how tall mount everest is?',
       },
     ],
   });
 
+  console.log(response.choices[0].message);
   console.log(response.choices[0].message.content);
 }
+
+/**
+ *  To understand token
+ */
+
+// function encodePrompt() {
+//   const prompt = 'how are you today';
+//   const encoder = encoding_for_model('gpt-3.5-turbo');
+//   const words = encoder.encode(prompt);
+//   console.log(words);
+// }
+
+// encodePrompt();
 
 main();
