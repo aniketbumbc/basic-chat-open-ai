b3d1a7361c17817491e5d6e5ec1c806032c8a398
aniket <aniket.umbc@gmail.com>
2024-08-12 12:44:47 +0530

    added different prompts method

---
+++ src/langchain/PrompTemplates.ts
@@ -0,0 +1,44 @@
+import { ChatOpenAI } from '@langchain/openai';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+
+const model = new ChatOpenAI({
+  modelName: 'gpt-3.5-turbo',
+  temperature: 0.8,
+});
+
+async function fromTemplate() {
+  const prompt = ChatPromptTemplate.fromTemplate(
+    'write a short description for product: {product_name}'
+  );
+
+  /**
+   *  Creating chain prompt-model
+   */
+
+  const chain = prompt.pipe(model);
+  const response = await chain.invoke({
+    product_name: 'Ferrari',
+  });
+
+  console.log(response.content);
+}
+
+async function fromMessage() {
+  const prompt = ChatPromptTemplate.fromMessages([
+    ['system', 'Write a short description for 10 years boy'],
+    ['human', '{product_name}'],
+  ]);
+
+  /**
+   *  Creating chain prompt-model
+   */
+
+  const chain = prompt.pipe(model);
+  const response = await chain.invoke({
+    product_name: 'Ferrari',
+  });
+
+  console.log(response.content);
+}
+
+fromMessage();
